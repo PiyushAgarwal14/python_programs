46429a84bf1c4ff8d7917768dfd26b06d0c5d48b
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2023-05-09 21:55:57 +0530

    Add files via upload

---
+++ python_html/Own_Server/main.py
@@ -0,0 +1,16 @@
+def greetings(name):
+    print(f'Hi, {name}')
+
+greetings('Piyush Agarwal')
+
+def addsum():
+    a = 10
+    b = 45
+    print(a+b)
+addsum()
+
+# def piy():
+#     a = int(input("Enter a number"))
+#     b = int(input("\nEnter another number"))
+#     print(a+b)
+# piy()
