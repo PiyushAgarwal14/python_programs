c6762dbed9ddf22f607b3c1cdfb8878df203bf44
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2023-12-08 18:42:18 +0530

    Add files via upload

---
+++ Python_basic/main.py
@@ -0,0 +1,79 @@
+# print("hello")
+
+# name = input("Enter your name?")
+# print('hello ' + name)
+
+# num1 =  int(input('Enter a integer'))
+# print(num1)
+
+# num2 = float(input('Enter a float'))
+# print(num2)
+
+# num3 = str(input('Enter a string'))
+# print (num3)
+
+# ********************************************
+
+# fundamental datatypes
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# dict
+# set
+# complex
+
+# ************************************************
+# print("Addition")
+# print (2 + 4)
+
+# print("Subtraction")
+# print(2-4)
+
+# print("Multiply")
+# print(2 * 3)
+
+# print("Dividend")
+# print(2 / 4)
+
+# square root
+# print( 2 ** 2)
+
+# mod
+# print (12 % 2)
+
+# type
+# print (type(2 + 5))
+# print (type(2 * 3.2))
+# print(type('piyush'))
+
+# print(10 / 3)
+# print (10 // 3)
+
+# **round of**
+# print(round(3.1))
+
+# square root
+# print(pow(4, 2))
+
+# print(round(3.8))
+
+# absolute value of argument
+# print(abs(-5))
+
+# *** operator precedence ***
+# print(12 + 6 * 3)
+
+# print((12*12) - 4 ** 2)
+# ()
+# **
+#  * /
+#  + -
+#  BODMAS bracket, exponent, multiply, divide, add, subtract
+
+# print(bin(6))
+# print(hex(17))
+
+# print(int('0b110', 2))
