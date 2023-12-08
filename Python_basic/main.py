f270d43d278759cb06afefb98b7a97c61485a49f
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2023-12-09 00:48:13 +0530

    Update main.py

--- Python_basic/main.py
+++ Python_basic/main.py
@@ -77,3 +77,157 @@
 # print(hex(17))
 
 # print(int('0b110', 2))
+
+# (*** 08/12/2023 ****)
+
+# print(2**3)
+# print(pow(2, 3))
+
+# (**** variables are snake_case (no space) *****)
+# (*** variables start with lower case or underscore  ***)
+# (*** variables are case sensitive ***))
+# (*** variables can be letter, numbers, underscores)
+# (*** Dont overwrite keywords ***)
+
+# user = 10
+# print(user)
+
+# user_1 = 11
+# print(user_1)
+
+# _user_1 = 12
+# print(_user_1)
+
+# Piyush = 55
+# print(Piyush)
+
+# a,b,c = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+
+# print(a,b,c)
+
+# (**** augmented assignment operators ****)
+
+# some_value = 5
+# print(some_value)
+
+# some_value = 5+2
+# print(some_value)
+
+# some_value = some_value + 3
+# print(some_value)
+
+# some_value += 4
+# print(some_value)
+
+# some_value -= 6
+# print(some_value)
+
+# some_value *= 2
+# print(some_value)
+
+# some_value /= 2
+# print(some_value)
+
+# some_value //=2
+# print(some_value)
+
+# some_value %= 2
+# print(some_value)
+
+# some_value **= 2
+# print(some_value)
+
+# print(type("hello world, this is a string"))
+
+# long_string = '''
+#   Amazing
+
+#   ^ ^
+#    -
+#  cool
+
+#  # #
+#  ---
+# '''
+# print(long_string)
+
+# first_name = 'Piyush'
+# last_name = 'Agarwal'
+# full_name = first_name + ' ' + last_name
+# print(full_name)
+
+# String concationation
+# print('Piyush ' + 'Agarwal')
+
+# print(str(55))
+# print(type(str(55)))
+
+# print(type(int(str(100))))
+
+# a = str(55)
+# b = int(a)
+# c = type(b)
+# print(c)
+
+# a = 'hello'
+# b = 'HELLO'
+# b = b.lower()
+
+## print(b)
+
+# if a == b:
+#   print('true')
+# else:
+#   print('false')
+
+# esacpe sequence
+
+# weather = "It\'s \"cold\" nignt"
+# print(weather)
+
+# weather = "It\\s \"cold\" nignt"
+# print(weather)
+
+# weather = "\t It\'s \"cold\" nignt \n hope you have a good day"
+# print(weather)
+
+# name = str('Piyush')
+# age = int(22)
+# print('Welcome ' + name + '. You are ' + str(age) + ' year old')
+
+# formatted string
+# print(f'Welcome {name} You are {age} years old')
+
+# selfish = 'me me me'
+# #          01234567
+# print(selfish[7])
+
+# arr = '01234567'
+# # # [start:stop:stepover]
+#  print(arr[0:8:2])
+
+# print(arr[::-1])
+
+# a = 'reviver'
+# a[::-1]
+
+# if a == a[::-1]:
+#   print('palindrome')
+# else:
+#   print('not palindrome')
+
+
+# arr = '1234567'
+# print(len(arr))
+
+# word = 'hello my name is'
+# print(word.upper())
+# print(word.capitalize())
+# print(word.lower())
+
+# print(word.find('is'))
+# print(word.replace('my' , 'Piyush'))
+# (*** strings are immutable ***)
