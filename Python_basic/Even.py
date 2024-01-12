dbdb4adfcdb486d77e59a57d0cf0e1e6f53c1a68
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2024-01-12 23:10:13 +0530

    Create Even.py

---
+++ Python_basic/Even.py
@@ -0,0 +1,22 @@
+      # Method 1
+
+# a = int(input('Enter a number: \n'))
+# if (a % 2 == 0):
+#     print('even')
+# else:
+#     print('odd')
+
+
+
+
+    # Method 2
+
+# list = []
+# n = int(input("Enter length of the list: \n"))
+# for i in range(0,n):
+#     i = int(input(f"Enter {i+1} elements in the list: \n"))    
+#     list.append(i)
+# print("\n", list, "\n")
+# for j in list:
+#     if(j % 2 == 0):
+#         print(j, end="")
