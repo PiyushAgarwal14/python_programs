4e5a16504772a2fe90cb3958898721e3c3f73942
Piyush Agarwal <87847452+PiyushAgarwal14@users.noreply.github.com>
2024-01-14 15:24:45 +0530

    Create Combine_two_dict.py

---
+++ Python_basic/Combine_two_dict.py
@@ -0,0 +1,9 @@
+dict1 = {"fruits": "apple, banana",
+         "car": "honda, suzuki"
+        }
+dict2 = {"animal":"tiger, lion",
+         "birds": "eagle, ostrich"
+        }
+
+dict1.update(dict2)
+print(dict1)
